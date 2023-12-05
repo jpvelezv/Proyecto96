84d7f0747b46caab2346c72bb8f3407fd22a947c
Juan Pablo Vélez Valencia <jpvelezv@gmail.com>
2023-12-05 18:16:27 -0500

    Commit
    Commit

---
+++ game_login.js
@@ -0,0 +1,11 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Establece "player1_name" usando la función localStorage.setItem()
+	  //Establece "player2_name" usando la función localStorage.setItem()
+
+    window.location = "game_page.html";
+}
+
